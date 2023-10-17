4ad9758065ef52bf8959841ab813153234794fb0
Pranav Konde <pranavkonde2020@gmail.com>
2023-10-17 22:45:52 +0530

    added payroll

--- api/payroll.js
+++ api/payroll.js
@@ -2,15 +2,16 @@ const express = require("express");
 const shop = require("../db/models/shop");
 const employee = require("../db/models/employee");
 const task = require("../db/models/task");
+const payroll = require("../db/models/payroll");
 const saltRounds = 10;
 
 
-// create task
-router.post("/task", async (req, res) => {
-    var task_details = req.body;
-    var response = await task.create(task_details);
+// create payroll
+router.post("/payroll", async (req, res) => {
+    var payroll_details = req.body;
+    var response = await payroll.create(payroll_details);
     if (!response || !response?._id) {
-      return res.status(500).json({ message: "Error in creating task!" });
+      return res.status(500).json({ message: "Error in creating payroll!" });
     }
     return res.status(200).send({ message: "success" });
   });
